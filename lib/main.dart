e072a4d16db35785bc80d56e871bcda44b4df138
Stefan Groneberg <stefan.groneberg@gmx.de>
2025-07-23 17:11:20 +0200

    adding appBar variation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:image_gallery_app/screen/details.dart';
 import 'package:image_gallery_app/widget/gallery.dart';
 import 'package:image_gallery_app/widget/profile.dart';
 
@@ -18,16 +17,24 @@ class _MainAppState extends State<MainApp> {
   int _selectedIndex = 0;
   final List<Widget> _pages = const [
     Gallery(),
-    // Detail(),
     Profile(),
-    // Add other pages here if needed
   ];
 
   final List<NavigationDestination> navigationDestinationList = [
     NavigationDestination(icon: Icon(Icons.image), label: 'Gallery'),
-    // NavigationDestination(icon: Icon(Icons.details), label: 'Details'),
     NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
   ];
+
+  final List<AppBar> appBarList = [AppBar(
+          title: Text('Gallery'),
+          backgroundColor: Color.fromRGBO(167, 19, 19, 1.0),
+          foregroundColor: Color.fromRGBO(193, 199, 48, 1.0),
+        ), 
+        AppBar(
+          title: Text('Profile'),
+          backgroundColor: Color.fromRGBO(193, 199, 48, 1.0),
+          foregroundColor: Color.fromRGBO(43,83,167, 1.0),
+        )];
 
   void onDestinationSelected(int index) {
     setState(() {
@@ -40,11 +47,7 @@ class _MainAppState extends State<MainApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Gallery'),
-          backgroundColor: Color.fromRGBO(167, 19, 19, 1.0),
-          foregroundColor: Color.fromRGBO(193, 199, 48, 1.0),
-        ),
+        appBar: appBarList[_selectedIndex],
         body: _pages[_selectedIndex],
         bottomNavigationBar: NavigationBar(
           onDestinationSelected: onDestinationSelected,
